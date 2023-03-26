a28f7c5d8598e6e07845bfec5fcec4c400294f18
Vladislav <vladlipchey05@gmail.com>
2023-03-26 13:30:22 +0300

    return roles to frontend

--- app/Http/Controllers/Personal/IndexController.php
+++ app/Http/Controllers/Personal/IndexController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers\Personal;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
     public function __invoke()
     {
-        return 'suuui';
+        //return 'suuui';
+        $roles=User::getRole();
+        return $roles;
     }
 }
