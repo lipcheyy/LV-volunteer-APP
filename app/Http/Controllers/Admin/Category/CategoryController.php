3f83e84a871d41851ade35d6ea44531b155868c8
Vladislav <vladlipchey05@gmail.com>
2023-04-12 21:15:54 +0300

    create category in database

--- app/Http/Controllers/Admin/Category/CategoryController.php
+++ app/Http/Controllers/Admin/Category/CategoryController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Admin\Category;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Category\StoreRequest;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
@@ -10,7 +12,11 @@ class CategoryController extends Controller
     public function index(){
         return 'siiiu';
     }
-    public function store(){
-
+    public function store(StoreRequest $request){
+        $data=$request->validated();
+        Category::firstOrCreate([
+            'title'=>$data['title']
+        ]);
+        return $data;
     }
 }
