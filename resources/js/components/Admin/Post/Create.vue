18cf90eb86d406049f24831865e41a4da7c9a283
Vladislav <vladlipchey05@gmail.com>
2023-04-24 16:11:49 +0300

    create post component

---
+++ resources/js/components/Admin/Post/Create.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: "PostCreate"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/Admin/Post/PostIndex.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <post-create></post-create>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "PostIndex",
+    components: {}
+
+}
+</script>
+
+<style scoped>
+
+</style>
