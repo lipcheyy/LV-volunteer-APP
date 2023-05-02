2e5132e9d145056ffbc3a591e5cf3ce0040b83f5
Vladislav <vladlipchey05@gmail.com>
2023-05-02 23:23:48 +0300

    feat: initialize dropzone

--- resources/js/components/FindRelative/FindPersonRequestForm.vue
+++ resources/js/components/FindRelative/FindPersonRequestForm.vue
@@ -4,19 +4,28 @@
         <input v-model="name" type="text">
         write about person
         <textarea v-model="about"></textarea>
-
+        <div ref="dropzone"></div>
     </div>
 </template>
 
 <script>
+import Dropzone from 'dropzone'
 export default {
     name: "FindPersonRequestForm",
     data(){
         return{
             name:'',
             about:'',
-
+            dropzone:null
         }
+    },
+    mounted() {
+        this.dropzone=new Dropzone(
+            this.$refs.dropzone,
+            {
+                url:'/sdf'
+            }
+        )
     }
 }
 </script>
