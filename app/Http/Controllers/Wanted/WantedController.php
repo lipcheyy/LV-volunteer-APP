eea89c38f86b3805f46aed1cd0ac6c31c8b66e3c
Vladislav <vladlipchey05@gmail.com>
2023-05-03 23:24:47 +0300

    store wanted request text data

--- app/Http/Controllers/Wanted/WantedController.php
+++ app/Http/Controllers/Wanted/WantedController.php
@@ -4,12 +4,20 @@ namespace App\Http\Controllers\Wanted;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Wanted\StoreRequest;
+use App\Models\Wanted;
 use Illuminate\Http\Request;
 
 class WantedController extends Controller
 {
     public function store(StoreRequest $request){
         $data=$request->validated();
-        return dump($data);
+        $images=$data['images'];
+        unset($data['images']);
+        $wanted=Wanted::create($data);
+        foreach ($images as $image)
+        {
+
+        }
+        return dump($images);
     }
 }
