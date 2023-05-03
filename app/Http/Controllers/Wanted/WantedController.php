0c0f735d5502f77b7199613ce6cd8dec5e94ca8f
Vladislav <vladlipchey05@gmail.com>
2023-05-03 23:34:19 +0300

    store wanted request persons image

--- app/Http/Controllers/Wanted/WantedController.php
+++ app/Http/Controllers/Wanted/WantedController.php
@@ -4,8 +4,11 @@ namespace App\Http\Controllers\Wanted;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Wanted\StoreRequest;
+use App\Models\Image;
 use App\Models\Wanted;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class WantedController extends Controller
 {
@@ -16,7 +19,16 @@ class WantedController extends Controller
         $wanted=Wanted::create($data);
         foreach ($images as $image)
         {
-
+            $imageName=md5(Carbon::now() . '_'. $image->getClientOriginalName())
+                .'.'.$image->getClientOriginalExtension();
+            $imagePath=Storage::disk('public')->putFileAs('/images',$image,$imageName);
+            Image::create(
+                [
+                    'path'=>$imagePath,
+                    'url'=>url('/storage/'.$imagePath),
+                    'wanted_id'=>$wanted->id,
+                ]
+            );
         }
         return dump($images);
     }
